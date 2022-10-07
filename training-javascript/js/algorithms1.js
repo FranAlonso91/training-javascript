6b0651a7ed8b1f66e6131ec73abe6b29b24cc4bb
Fran Alonso <franciscojalonso91@gmail.com>
2022-10-07 20:20:40 +0200

    Starting exercises 3 JS

---
+++ training-javascript/js/algorithms1.js
@@ -0,0 +1,22 @@
+//EJERCICIO 1
+/* const fecha = new Date();
+let mes = fecha.getMonth();
+console.log(fecha) */
+//EJERCICIO 2
+let tellmeMonth = prompt("Please, tell me a month");
+if (
+  tellmeMonth == "January" ||
+  "February" ||
+  "March" ||
+  "April" ||
+  "May" ||
+  "June" ||
+  "July" ||
+  "August" ||
+  "September" ||
+  "October" ||
+  "November" ||
+  "December"
+);
+{console.log(tellmeMonth)
+}
