f32a46d00f51595c286121cfd49b8ef5400f6071
FranAlonso91 <franciscojalonso91@gmail.com>
2022-10-08 14:10:43 +0200

    Saturday 8 JS training

--- training-javascript/js/algorithms1.js
+++ training-javascript/js/algorithms1.js
@@ -3,20 +3,78 @@
 let mes = fecha.getMonth();
 console.log(fecha) */
 //EJERCICIO 2
-let tellmeMonth = prompt("Please, tell me a month");
-if (
-  tellmeMonth == "January" ||
-  "February" ||
-  "March" ||
-  "April" ||
-  "May" ||
-  "June" ||
-  "July" ||
-  "August" ||
-  "September" ||
-  "October" ||
-  "November" ||
-  "December"
-);
-{console.log(tellmeMonth)
+/* let monthName = prompt("Please, write the chosen month.");
+switch (monthName) {
+  case "January":
+    console.log("January has 31 days");
+    break;
+  case "February":
+    console.log("February has 28 days");
+    break;
+  case "March":
+    console.log("March has 31 days");
+    break;
+  case "April":
+    console.log("April has 30 days");
+    break;
+  case "May":
+    console.log("May has 31 days");
+    break;
+  case "June":
+    console.log("June has 30 days");
+    break;
+  case "July":
+    console.log("July has 31 days");
+    break;
+  case "August":
+    console.log("August has 31 days");
+    break;
+  case "September":
+    console.log("September has 30 days");
+    break;
+  case "October":
+    console.log("October has 31 days");
+    break;
+  case "November":
+    console.log("November has 30 days");
+    break;
+  case "December":
+    console.log("December has 31 days");
+    break;
+  default:
+    console.log("That’s not a real month!");
+    break;
+} */
+//EJERCICIO 3
+/* let temperature = prompt("Please, what is the temperature outside?.");
+console.log(temperature);
+if (temperature < 15) {
+  alert("Warm up");
+} else if (temperature ==15 || temperature <= 25) {
+  alert("Enjoy the weather!");
+}
+else if (temperature > 25) {
+  alert("Cool down")
+}
+ */
+//EJERCICIO 4
+/* switch (isGreen) {
+  case "True":
+    console.log("Cross the road");
+    break;
+  case "False":
+    console.log("Please await");
+    break;
+} */
+//EJERCICIO 5
+
+//EJERCICIO 6
+let email = "tolkien@lordofrings.com";
+let password = "aNBR6ZeWPY";
+let eMail = prompt ("Please insert your email")
+if(email == true && password == true) {
+  alert("Welcome Mr.Tolkien");
+}
+else{
+  alert("Wrong password or email")
 }
